f146373861562988eb250535e694a671f7d42a61
Aditya.Pandit <adpndt@gmail.com>
2025-05-20 12:01:02 +0530

    added favicon

--- src/main/java/com/meetingBuddy/meetingBuddy/config/SecurityConfig.java
+++ src/main/java/com/meetingBuddy/meetingBuddy/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/meetings/today").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/login", "/register", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/images/**","/login", "/register", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
